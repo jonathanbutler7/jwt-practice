849bac16d2677075cda75e704cb124002ef914bd
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-12-14 21:11:31 -0800

    made changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = new express();
 const cors = require('cors');
 const parser = require('body-parser');
 const routes = require('./routes/index');
-const passport = require('./config/passport')
+const passport = require('./config/passport');
 app.use(cors());
 app.use(parser.json());
 app.use(passport.initialize());
@@ -18,6 +18,7 @@ app.use(
 app.use(routes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
